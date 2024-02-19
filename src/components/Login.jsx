5e7c48c9ff24b78de11cf831ed583036ec118f12
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-02-20 00:19:31 +0100

    Forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,34 @@
+import { useRef } from "react";
+
 export default function Login() {
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("hello");
+  const email = useRef();
+  const password = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(email.current.value);
+    console.log(password.current.value);
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button" onClick={handleSubmit}>
-          Login
-        </button>
+        <button className="button">Login</button>
       </p>
     </form>
   );
